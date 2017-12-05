25b7223d8dfc6d4340e7d50e8a2aa87a0cd0d24d
Hammad Siddiqui <hammadsiddiqui777@gmail.com>
2017-12-05 12:23:47 +0500

    Project setup

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+import VideoSurv
+
+#cap = cv2.VideoCapture()
+#videoSurveillance = VideoSurv()
+
+
+def show_webcam(mirror=False):
+    cam = cv2.VideoCapture(0)
+    while True:
+        ret_val, img = cam.read()
+        if mirror:
+            img = cv2.flip(img, 1)
+        cv2.imshow('Video Feed', img)
+        if cv2.waitKey(1) == 27:
+            break  # esc to quit
+    cv2.destroyAllWindows()
+
+
+def main():
+    show_webcam(mirror=True)
+
+
+if __name__ == '__main__':
+    main()
